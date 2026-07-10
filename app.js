b02dab5ca06becf8514e88926527a4861c659e7a
Pravin Jegan <pravinramjegan@gmail.com>
2026-07-10 01:03:57 -0400

    Day 2 did js things created camera which takes the output and puts it into the video obejct in html, also used a async fucntion to get the ai model from the wb and load it

---
+++ app.js
@@ -0,0 +1,44 @@
+import{
+    PoseLandmarker,
+    FilesetResolver,
+    DrawingUtils,
+} from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest";
+
+const video = document.getElementById('camera');
+const line_overlay = document.getElementById('line_overlay');
+
+const canvasCtx = line_overlay.getContext('2d');
+
+let poseLandmarker;
+let runningMode = "VIDEO";
+let lastVideoTime = -1;
+let peopleCount = 1;
+
+
+async function posetracker() {
+    const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
+    );
+    poseLandmarker = await PoseLandmarker.createFromOptions(vision, {
+        baseOptions: {
+            modelAssetPath: "https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task",
+            delegate: "GPU"
+        },
+        runningMode: runningMode,
+        numPoses: peopleCount
+    });
+
+    console.log("AI Model has been loade from online");
+
+    startcamera();
+}
+
+function startcamera(){
+navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+    video.srcObject = stream;
+    //video.addEventListener("loadeddata", drawbones) // this is for later it just saying right after we get the video to do the ai over lay draw bones iswhats gonna draw bones 
+
+});
+
+}
+
+posetracker();
